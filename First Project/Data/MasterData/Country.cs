899935bb34a2cdaafcad5e404ec0999122ca2695
Md. Shariful Islam <sharifulislam2302963@outlook.com>
2023-02-15 23:15:17 +0600

    Master Data Add

---
+++ First Project/Data/MasterData/Country.cs
@@ -0,0 +1,24 @@
+﻿using First_Project.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FADMS.Data.Entity.Master
+{
+    public class Country : Base
+    {       
+        [Column(TypeName = "NVARCHAR(20)")]
+        public string countryCode { get; set; }        
+        [Column(TypeName = "NVARCHAR(120)")]
+        public string countryName { get; set; }
+        [Column(TypeName = "NVARCHAR(120)")]
+        public string countryNameBn { get; set; }
+        [Column(TypeName = "NVARCHAR(20)")]
+        public string shortName { get; set; }
+        [Column(TypeName = "NVARCHAR(10)")]
+        public string isActive { get; set; }
+        [Column(TypeName = "NVARCHAR(120)")]
+        public string latitude { get; set; }
+        [Column(TypeName = "NVARCHAR(120)")]
+        public string longitude { get; set; }
+    }
+}
---
+++ First Project/Data/MasterData/Occupation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using First_Project.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FADMS.Data.Entity.Master
+{
+    public class Occupation:Base
+    {
+        [Required]
+        [Column(TypeName = "NVARCHAR(180)")]
+        public string occupationName { get; set; }
+        [Column(TypeName = "NVARCHAR(120)")]
+        public string occupationShortName { get; set; }
+        public int? shortOrder { get; set; }
+    }
+}
